e404ea8726668e3cf9c74bf9fa34399fecf07761
mazyukv <mazyuk.v@yandex.ru>
2024-11-21 20:27:09 +0100

    new course

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template
-
-
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', current_title='Custom Title')
+@app.route('/<name>')
+def index(name):
+    return '<h1>Hello {}!</h1>'.format(name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
